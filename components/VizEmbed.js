18c9fbe831b13bbe637440bb6196fbc2b0542d35
Calix Huang <calix.huang1@gmail.com>
2020-06-27 11:58:16 -0700

    vizembed component

---
+++ components/VizEmbed.js
@@ -0,0 +1,5 @@
+export default ({ url }) => {
+  return (
+    <iframe src={url}></iframe>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import VizEmbed from '../components/VizEmbed'
 
 export default function Home() {
   return (
     <Layout>
       <h1>CVT Super Dashboard</h1>
+
+      <VizEmbed url="https://www.bing.com/covid/local/unitedstates" />
     </Layout>
   )
 }
